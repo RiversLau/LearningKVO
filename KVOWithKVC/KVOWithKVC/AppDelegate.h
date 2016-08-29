04531c2c40197c791f1a891337c45a7e739a515c
RiversLau <zhaoxiang0805@126.com>
2016-08-29 17:53:56 +0800

    项目创建

---
+++ KVOWithKVC/KVOWithKVC/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  KVOWithKVC
+//
+//  Created by shine-team2 on 16/8/29.
+//  Copyright © 2016年 ShowTime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ KVOWithKVC/KVOWithKVC/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KVOWithKVC
+//
+//  Created by shine-team2 on 16/8/29.
+//  Copyright © 2016年 ShowTime. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
